9dfa83ea559e1894934b4b474cd01b27c937ad21
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-08-15 15:32:59 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -17,7 +17,8 @@ BreachType classifyTemperatureBreach(CoolingType coolingType, double temperature
 
 typedef enum {
   TO_CONTROLLER,
-  TO_EMAIL
+  TO_EMAIL,
+   UNKNOWN_TARGET
 } AlertTarget;
 
 typedef struct {
