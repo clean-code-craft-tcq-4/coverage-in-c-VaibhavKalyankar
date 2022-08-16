28c1f6dd5cf835764583ab19b3f53079c052df21
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-08-16 20:07:49 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -18,19 +18,21 @@ BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
   int lowerLimit = 0;
   int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
+   if(coolingType == PASSIVE_COOLING)
+   {
       lowerLimit = 0;
       upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
+   }
+   elseif (coolingType == HI_ACTIVE_COOLING)
+   {
       lowerLimit = 0;
       upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
+   }
+    else
+    {
       lowerLimit = 0;
       upperLimit = 40;
-      break;
+    }
   }
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
@@ -67,16 +69,19 @@ void sendToController(BreachType breachType) {
 
 void sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
+  if(breachType == TOO_LOW) 
+  {
       printf("To: %s\n", recepient);
       printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
+  }
+  elseif (breachType == TOO_HIGH)
+  {
       printf("To: %s\n", recepient);
       printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
+  }
+  else
+  {
+    // do nothing
+  }
   }
 }
